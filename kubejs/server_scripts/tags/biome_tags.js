8e8d145da65a8f8f44e34bea2429fe9b92337559
Diamondback88 <23108617+Diamondback88@users.noreply.github.com>
2025-08-12 16:25:06 -0400

    Prevent ad astra and pneumaticcraft oil from spawning

--- kubejs/server_scripts/tags/biome_tags.js
+++ kubejs/server_scripts/tags/biome_tags.js
@@ -2,5 +2,12 @@
 
 ServerEvents.tags("worldgen/biome", (event) => {
   // Ice maze on Glacio
-  event.add("aquamirae:ice_maze", ["ad_astra:glacio_ice_peaks"])
-})
+  event.add("aquamirae:ice_maze", "ad_astra:glacio_ice_peaks")
+
+  // Prevent Ad Astra oil well from spawning
+  event.removeAll("ad_astra:has_structure/oil_well")
+
+  // Prevent PneumaticCraft oil lakes from spawning
+  event.removeAll("pneumaticcraft:has_surface_oil_lakes")
+  event.removeAll("pneumaticcraft:has_underground_oil_lakes")
+});
