d6e387ac83436301632e87a593cada8a543ea627
Hypermoron <dioticgaming@gmail.com>
2025-08-27 02:02:38 -0600

    Expand Morphing Tool support, clean up wrench items

---
+++ kubejs/client_scripts/jei_info.js
@@ -0,0 +1,18 @@
+// priority: 0
+
+JEIEvents.information(event => {
+  event.addItem('morphtool:tool', [
+      'A multitool that can hold wrench-like items from various mods.',
+      'Craft the Morphing Tool with a supported item to combine them.',
+      'The Tool will swap attachments automatically to match the block you are looking at.',
+      'To keep an attachment from morphing back, hold the Tool in your offhand.',
+      'You can also sneak + scroll to manually swap between attachments,',
+      'To remove an attachment, transform the Tool into the item you want to remove, then press sneak + drop.',
+      'Note that the Tool can only hold one item from each mod.',
+    ])
+
+  event.addItem('#perpendicular:morph_tools', [
+    'Works with the Morphing Tool. Combine with the Tool in a crafting grid to add as an attachment.'
+  ])
+
+})
