ba17f7309472a1f88a441412a2254d3792120c45
Hypermoron <dioticgaming@gmail.com>
2025-08-28 22:54:09 -0600

    Add missing ore tags

--- kubejs/server_scripts/tags/block_tags.js
+++ kubejs/server_scripts/tags/block_tags.js
@@ -3,4 +3,19 @@
 ServerEvents.tags('block', event => {
   // Allow Sully's Mod amber to prevent Alex's Caves dinosaurs from hatching
   event.add('alexscaves:stops_dinosaur_eggs', 'sullysmod:amber');
+
+  // Ores with missing tag
+  event.add('forge:ores', [
+    'spelunkery:andesite_zinc_ore',
+    'spelunkery:diorite_zinc_ore',
+    'spelunkery:granite_zinc_ore',
+    'aether_redux:veridium_ore',
+    'unusualprehistory:stone_amber_ore',
+    'unusualprehistory:deepslate_amber_ore',
+    'unusualprehistory:stone_opal_ore',
+    'unusualprehistory:deepslate_opal_ore',
+    'unusualprehistory:stone_tar_fossil',
+    'unusualprehistory:deepslate_tar_fossil',
+    'unusualprehistory:permafrost_fossil'
+  ]);
 });
